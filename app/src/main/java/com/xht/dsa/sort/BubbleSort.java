08f606fc79e4eb79a146058369b2cfc04575387f
许海涛 <xuhaitao@autohome.com.cn>
2020-02-10 09:21:04 +0800

    冒泡排序

---
+++ app/src/main/java/com/xht/dsa/sort/BubbleSort.java
@@ -0,0 +1,86 @@
+package com.xht.dsa.sort;
+
+import java.util.Arrays;
+
+/**
+ * Created by xht on 2020/2/5
+ */
+public class BubbleSort {
+
+    public static void sort1(int[] array) {
+        for (int i = 0; i < array.length - 1; i++) {
+            for (int j = 0; j < array.length - 1 - i; j++) {
+                int temp;
+                if (array[j] > array[j + 1]) {
+                    temp = array[j];
+                    array[j] = array[j + 1];
+                    array[j + 1] = temp;
+                }
+            }
+        }
+    }
+
+    public static void sort2(int[] array) {
+        for (int i = 0; i < array.length - 1; i++) {
+            boolean isSorted = true;
+            for (int j = 0; j < array.length - i - 1; j++) {
+                int temp;
+                if (array[j] > array[j + 1]) {
+                    temp = array[j];
+                    array[j] = array[j + 1];
+                    array[j + 1] = temp;
+                    isSorted = false;
+                }
+            }
+
+            if (isSorted) {
+                break;
+            }
+        }
+    }
+
+    public static void sort(int[] array) {
+        //记录最后一次交换的位置
+        int lastExchangeIndex = 0;
+        //无序数列的边界，每次比较只需要比到这里为止
+        int sortBorder = array.length - 1;
+
+        for (int i = 0; i < array.length - 1; i++) {
+            //有序标记，每一轮初始值都是true
+            boolean isSorted = true;
+            for (int j = 0; j < sortBorder; j++) {
+                int temp;
+                if (array[j] > array[j + 1]) {
+                    temp = array[j];
+                    array[j] = array[j + 1];
+                    array[j + 1] = temp;
+                    isSorted = false;
+                    lastExchangeIndex = j;
+                }
+            }
+
+            sortBorder = lastExchangeIndex;
+
+            if (isSorted) {
+                break;
+            }
+
+
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] array = new int[]{5, 8, 6, 3, 9, 2, 1, 7};
+
+        System.out.println(Arrays.toString(array));
+
+        sort1(array);
+
+        System.out.println(Arrays.toString(array));
+
+        sort2(array);
+
+        System.out.println(Arrays.toString(array));
+    }
+
+}
